1141c66ffd0f646074b5fa61c23107f72b505c9d
JohnnyWeymouth <johnnycweymouth@gmail.com>
2025-11-20 17:02:07 -0700

    Go part 2

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
+import os
 import platform
 from pathlib import Path
 from setuptools import setup
 from setuptools.command.build_py import build_py
+from wheel.bdist_wheel import bdist_wheel
 
 class BuildWithBinaries(build_py):
     """Custom build command to include Go binaries"""
@@ -13,12 +15,28 @@ class BuildWithBinaries(build_py):
         if not self.dry_run:
             binary_dir = Path("binaries")
             if binary_dir.exists():
-                target_dir = Path(self.build_lib) / "pairwise_name_comparator" / "bin"
+                target_dir = Path(self.build_lib) / "src" / "bin"
                 target_dir.mkdir(parents=True, exist_ok=True)
                 
                 for binary in binary_dir.glob("*"):
                     if binary.is_file():
                         self.copy_file(str(binary), str(target_dir / binary.name))
+                        # Make binary executable on Unix
+                        if binary.suffix != '.exe':
+                            os.chmod(str(target_dir / binary.name), 0o755)
+
+class BdistWheelPlatSpecific(bdist_wheel):
+    """Custom wheel command to mark the wheel as platform-specific"""
+    def finalize_options(self):
+        bdist_wheel.finalize_options(self)
+        # Mark as not pure Python since we include binaries
+        self.root_is_pure = False
+
+    def get_tag(self):
+        # This will be overridden by --plat-name in the build command
+        python, abi, plat = bdist_wheel.get_tag(self)
+        # Use py3 for Python version, none for ABI (no C extensions)
+        return 'py3', 'none', plat
 
 def get_binary_name():
     """Determine which binary to use based on platform"""
@@ -68,7 +86,7 @@ setup(
     ],
     # Include the binaries directory in the package
     package_data={
-        "pairwise_name_comparator": ["bin/*"],
+        "src": ["bin/*"],
     },
     include_package_data=True,
     cmdclass={
